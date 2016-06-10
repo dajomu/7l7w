08b4eed71d5ea37ee15804821db02feb574a6623
dajomu <mundaymeister@gmail.com>
2016-06-10 08:34:52 +0100

    [ruby] tried to extend array, hasnt worked

--- ruby/day3/acts_as_csv_module.rb
+++ ruby/day3/acts_as_csv_module.rb
@@ -1,3 +1,17 @@
+module SexierArray
+	include Array
+
+	def self.method_missing name, *args
+		column = name.to_s
+		puts column
+	end
+
+end
+
+class SuperArray # no inheritance! You can mix it in
+	include SexierArray
+end
+
 module ActsAsCsv
 	def self.included(base)
 		base.extend ClassMethods
@@ -5,18 +19,32 @@ module ActsAsCsv
 
 	module ClassMethods
 		def acts_as_csv
+			include Enumerable
 			include InstanceMethods
 		end
 	end
-	
+
 	module InstanceMethods
 		def read
-			@csv_contents = []
+			# note to self - create new class that extends array with method_missing 
+			@csv_contents = SuperArray.new
 			filename = self.class.to_s.downcase + '.txt'
 			file = File.new(filename)
 			@headers = file.gets.chomp.split(', ')
 			file.each do |row|
 				@csv_contents << row.chomp.split(', ')
+			end
+		end
+
+		def poo
+			puts 'poo'
+		end
+
+		def each(&block)
+			@csv_contents.each do |member|
+				puts member
+				puts block
+				block.call(member)
 			end
 		end
 
--- ruby/day3/csv_application.rb
+++ ruby/day3/csv_application.rb
@@ -1,14 +1,36 @@
 =begin
-Modify the CSV application to support an each method to return a CsvRow
-object. Use method_missing on that CsvRow to return the value for the column
-for a given heading.
+Modify the CSV application to support an each method 
+to return a CsvRow object. Use method_missing on that 
+CsvRow to return the value for the columnfor a given heading.
+
 For example, for the file:
 one, two
 lions, tigers
+
 allow an API that works like this:
 csv = RubyCsv.new
 csv.each {|row| puts row.one}
 This should print "lions" .
+=end
+
+=begin
+this is the example class with the missing method stuff
+class Roman
+	def self.method_missing name, *args
+		roman = name.to_s
+		roman.gsub!("IV", "IIII")
+		roman.gsub!("IX", "VIIII")
+		roman.gsub!("XL", "XXXX")
+		roman.gsub!("XC", "LXXXX")
+
+		(roman.count("I")
+		roman.count("V")
+		roman.count("X")
+		roman.count("L")
+		roman.count("C")
+	end
+end
+
 =end
 
 require "./acts_as_csv_module"
@@ -18,6 +40,9 @@ class RubyCsv # no inheritance! You can mix it in
 	acts_as_csv
 end
 
-m = RubyCsv.new
-puts m.headers.inspect
-puts m.csv_contents.inspect
+csv = RubyCsv.new
+puts csv.headers.inspect
+puts csv.csv_contents.inspect
+
+csv.poo
+csv.each {|row| puts row.one}
